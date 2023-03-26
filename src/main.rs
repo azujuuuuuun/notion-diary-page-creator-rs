2af5b7ec081d405ba30c27818cc955feb0cd0681
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-03-26 23:21:40 +0900

    Introduce service layer

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,29 @@ mod env;
 mod factory;
 mod http_client;
 mod notion;
+mod service;
 
-use std::{error::Error, process::exit};
+use std::error::Error;
 
 use dotenv::dotenv;
 use reqwest::Client as ReqwestClient;
 
-use crate::date::Date;
 use crate::env::Env;
-use crate::factory::NotionParamsFactory;
 use crate::http_client::HttpClient;
-use crate::notion::{NotionApiClient, NotionApiClientTrait};
+use crate::notion::NotionApiClient;
+use crate::service::{Service, ServiceTrait};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    println!("Creating diary page started.");
-
     dotenv().ok();
     let env = Env::load_env();
 
     let reqwest_client = ReqwestClient::new();
     let http_client = HttpClient::new(reqwest_client);
     let notion_client = NotionApiClient::new(&http_client, env.api_token);
+    let service = Service::new(notion_client);
 
-    let today = Date::today();
-
-    let params = NotionParamsFactory::build_query_database_params(&today);
-    let resp = notion_client
-        .query_database(&env.database_id, params)
-        .await?;
-    if !resp.results.is_empty() {
-        println!("Today's diary page was already created.");
-        exit(0);
-    }
-
-    let params = NotionParamsFactory::build_create_page_params(&env.database_id, &today);
-    notion_client.create_page(params).await?;
-
-    println!("Today's diary page was created successfully.");
+    service.create_diary_page(&env.database_id).await?;
 
     Ok(())
 }
---
+++ src/service.rs
@@ -0,0 +1,45 @@
+use std::error::Error;
+
+use async_trait::async_trait;
+
+use crate::date::Date;
+use crate::factory::NotionParamsFactory;
+use crate::notion::NotionApiClientTrait;
+
+#[async_trait]
+pub trait ServiceTrait {
+    async fn create_diary_page(&self, id: &str) -> Result<(), Box<dyn Error>>;
+}
+
+pub struct Service<C: NotionApiClientTrait> {
+    notion_client: C,
+}
+
+impl<C: NotionApiClientTrait> Service<C> {
+    pub fn new(notion_client: C) -> Self {
+        Service { notion_client }
+    }
+}
+
+#[async_trait]
+impl<C: NotionApiClientTrait + Sync + Send> ServiceTrait for Service<C> {
+    async fn create_diary_page(&self, id: &str) -> Result<(), Box<dyn Error>> {
+        println!("Creating diary page started.");
+
+        let today = Date::today();
+
+        let params = NotionParamsFactory::build_query_database_params(&today);
+        let resp = self.notion_client.query_database(id, params).await?;
+        if !resp.results.is_empty() {
+            println!("Today's diary page was already created.");
+            return Ok(());
+        }
+
+        let params = NotionParamsFactory::build_create_page_params(id, &today);
+        self.notion_client.create_page(params).await?;
+
+        println!("Today's diary page was created successfully.");
+
+        Ok(())
+    }
+}
